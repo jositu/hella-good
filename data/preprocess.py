2581ddfbc297328b807437b1a5ae81a8145b378f
andrewni <ajni@ucdavis.edu>
2018-03-17 15:23:23 -0700

    added some stuff

---
+++ data/preprocess.py
@@ -0,0 +1,45 @@
+import csv
+
+median_income_filename = 'MedianHouseholdIncome2015.csv'
+poverty_level_filename = 'PercentagePeopleBelowPovertyLevel.csv'
+education_level_filename = 'PercentOver25CompletedHighSchool.csv'
+police_killings_filename = 'PoliceKillingsUS.csv'
+diversity_filename = 'ShareRaceByCity.csv'
+
+state_data = {}
+city_data = {}
+
+with open(median_income_filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    a = csvreader.next()
+    print(a)
+
+# state_data = {
+#     'AL': {
+#         'MedianHouseholdIncome': ,
+#         'PovertyLevel': ,
+#         'EducationLevel': ,
+#         'White': ,
+#         'Black': ,
+#         'NativeAmerican': ,
+#         'Asian': ,
+#         'Hispanic': ,
+#         'NumPoliceKillings': ,
+#         'PoliceKillings': ,
+#     },
+# }
+
+# city_data = {
+#     'mycity': {
+#         'MedianHouseholdIncome': ,
+#         'PovertyLevel': ,
+#         'EducationLevel': ,
+#         'White': ,
+#         'Black': ,
+#         'NativeAmerican': ,
+#         'Asian': ,
+#         'Hispanic': ,
+#         'NumPoliceKillings': ,
+#         'PoliceKillings': ,
+#     },
+# }
