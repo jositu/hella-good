b3b836af2019244b38067b1c995e98047e27b13b
andrewni <ajni@ucdavis.edu>
2018-03-17 15:47:12 -0700

    js file for each vis

---
+++ map.js
@@ -0,0 +1,6 @@
+let marginMap = { top: 10, right: 10, bottom: 50, left: 120 };
+let fullWidthMap = 900;
+let fullHeightMap = 500;
+let widthMap = fullWidthMap - marginMap.right - marginMap.left;
+let heightMap = fullHeightMap - marginMap.top - marginMap.bottom;
+
---
+++ pcoords.js
@@ -0,0 +1,5 @@
+let marginPcoords = { top: 100, right: 100, bottom: 100, left: 100 };
+let fullWidthPcoords = 1300;
+let fullHeightPcoords = 1000;
+let widthPcoords = fullWidthPcoords - marginPcoords.right - marginPcoords.left;
+let heightPcoords = fullHeightPcoords - marginPcoords.top - marginPcoords.bottom;
