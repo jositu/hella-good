8594a58d0630300a7ee98242af3de74d2cf35771
jositu <jsitu@ucdavis.edu>
2018-03-18 15:57:26 -0700

    formatted code

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 // define the dimensions of the graph display wrt window
-let marginMap = { top: 10, right: 10, bottom: 50, left: 120 };
+let marginMap = {
+    top: 10,
+    right: 10,
+    bottom: 50,
+    left: 120
+};
 let fullWidthMap = 600;
 let fullHeightMap = 500;
 let widthMap = fullWidthMap - marginMap.right - marginMap.left;
 let heightMap = fullHeightMap - marginMap.top - marginMap.bottom;
 
-let marginPcoords = { top: 40, right: 20, bottom: 10, left: 50 };
+let marginPcoords = {
+    top: 40,
+    right: 20,
+    bottom: 10,
+    left: 50
+};
 let fullWidthPcoords = window.innerWidth * 0.5;
 let fullHeightPcoords = window.innerHeight * 0.5;
 let widthPcoords = fullWidthPcoords - marginPcoords.right - marginPcoords.left;
@@ -17,7 +27,6 @@ d3.csv('./data/cityData.csv', function (data) {
         function () {
             PCoords.update(data, heatMap.selectedStates);
         });
-
     var PCoords = new Parallel_Coords(d3.select("#pcoords-holder"), data, ["AL"]);
     var Sankey = new SankeyDiagram(d3.select("#sankey-holder"), data);
 })
