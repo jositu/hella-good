641cf2125d782330c76f264994bdd82642771221
jositu <jsitu@ucdavis.edu>
2018-03-17 20:51:23 -0700

    added parallel coordinates
    added grids template

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 // define the dimensions of the graph display wrt window
 let marginMap = { top: 10, right: 10, bottom: 50, left: 120 };
-let fullWidthMap = 900;
+let fullWidthMap = 600;
 let fullHeightMap = 500;
 let widthMap = fullWidthMap - marginMap.right - marginMap.left;
 let heightMap = fullHeightMap - marginMap.top - marginMap.bottom;
 
 let marginSankey = { top: 0, right: 0, bottom: 0, left: 0 };
-let fullWidthSankey = 1200;
+let fullWidthSankey = 900;
 let fullHeightSankey = 500;
 let widthSankey = fullWidthSankey - marginSankey.right - marginSankey.left;
 let heightSankey = fullHeightSankey - marginSankey.top - marginSankey.bottom;
 
-let marginPcoords = { top: 100, right: 100, bottom: 100, left: 100 };
-let fullWidthPcoords = 1300;
-let fullHeightPcoords = 1000;
+let marginPcoords = { top: 40, right: 20, bottom: 10, left: 50 };
+// let fullWidthPcoords = 900;
+// let fullHeightPcoords = 500;
+let fullWidthPcoords = window.innerWidth * 0.5;
+let fullHeightPcoords = window.innerHeight * 0.5;
 let widthPcoords = fullWidthPcoords - marginPcoords.right - marginPcoords.left;
 let heightPcoords = fullHeightPcoords - marginPcoords.top - marginPcoords.bottom;
 
--- pcoords.js
+++ pcoords.js
@@ -1,5 +1,135 @@
-let marginPcoords = { top: 100, right: 100, bottom: 100, left: 100 };
-let fullWidthPcoords = 1300;
-let fullHeightPcoords = 1000;
-let widthPcoords = fullWidthPcoords - marginPcoords.right - marginPcoords.left;
-let heightPcoords = fullHeightPcoords - marginPcoords.top - marginPcoords.bottom;
+// let widthPcoords = fullWidthPcoords - marginPcoords.right - marginPcoords.left;
+// let heightPcoords = fullHeightPcoords - marginPcoords.top - marginPcoords.bottom;
+
+d3.csv('data/cityData.csv', function(data) {
+	console.log(data);
+	var parallel_coordinates = new PCoords(d3.select("#pcoords-holder"), data, "AL");
+});
+
+function PCoords(container, data, initialState) {
+	console.log('main.js');
+
+	// set margins of the graph
+	// var margin = {top: 100, right: 20, bottom: 10, left: 40};
+	// var width = 900 - margin.left - margin.right;
+	// var height = 825 - margin.top - margin.bottom;
+
+	var margin = marginPcoords;
+	var width = widthPcoords;
+	var height = heightPcoords;
+
+	// create the svg canvas
+	var svg = d3.select('#pcoords-holder')
+		.append('svg')
+		.attr('width', width + margin.left + margin.right)
+		.attr('height', height + margin.top + margin.bottom)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	// create x and y variables
+	var x = d3.scaleBand().range([0,width]).padding(.1);
+	var y = {};
+	var dragging = {};
+	var line = d3.line();
+
+	// find position of an axis
+	function position(d) {
+	    var v = dragging[d];
+	    return v == null ? x(d) : v;
+	}
+
+	function transition(g) {
+	  return g.transition().duration(200);
+	}
+
+	// path of a given data point
+	function path(d) {
+	    return line(dimensions.map(function(p) { return [position(p), y[p](d[p])]; }));
+	}
+
+    this.update = function(data, targetState){
+	        svg.selectAll("*").remove();
+
+		var filteredData = data.filter(function(d) {
+			if (d["Geographic Area"] === targetState)
+				return d;
+		});
+
+		var ftotal = Object.keys(filteredData).length;
+
+		var total = Object.keys(data).length;
+
+        // find the dimensions (axes)
+    	x.domain(dimensions = d3.keys(data[0]).filter(function(d) {
+            return d != "Geographic Area" && d != "City" && (y[d] = d3.scaleLinear()
+                .domain(d3.extent(data, function(p) {return +p[d];}))
+                .range([height, 0]));
+        }));
+
+        // add grey background lines for context (all data)
+        var background = svg.append("g")
+            .attr("class", "background")
+            .selectAll("path")
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("opacity", 0.08);
+
+        // add blue foreground lines for selection (filtered data)
+        var foreground = svg.append("g")
+            .attr("class", "foreground")
+            .selectAll("path")
+            .data(filteredData)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("opacity", +((total-ftotal)/total)*0.1 );
+      
+        // add a group element for each dimension.
+        var g = svg.selectAll(".dimension")
+            .data(dimensions)
+            .enter()
+            .append("g")
+            .attr("class", "dimension")
+            .attr("transform", function(d) {return "translate(" + x(d) + ")";})
+            // ability to switch axes
+            .call(d3.drag()
+                .on("start", function(d) {
+                  dragging[d] = x(d);
+                  background.attr("visibility", "hidden");
+                })
+                .on("drag", function(d) {
+                    dragging[d] = Math.min(width, Math.max(0, d3.event.x));
+                    foreground.attr("d", path);
+                    dimensions.sort(function(a, b) {return position(a) - position(b);});
+                    x.domain(dimensions);
+                g.attr("transform", function(d) {return "translate(" + position(d) + ")";})
+                })
+                .on("end", function(d) {
+                    delete dragging[d];
+                    transition(d3.select(this)).attr("transform", "translate(" + x(d) + ")");
+                    transition(foreground).attr("d", path);
+                    background.attr("d", path)
+                        .transition()
+                        .delay(500)
+                        .duration(0)
+                        .attr("visibility", null);
+                }));
+
+        // add ticks for axes
+        g.append("g")
+            .attr("class", "axis")
+            .each(function(d) { d3.select(this).call(d3.axisLeft(y[d]).ticks(5)); });
+
+        // add genre labels for each axis
+        g.append("text")
+            .style("text-anchor", "middle")
+            .attr("y", -20)
+            .attr("font-size", 12)
+            .text(d => d);
+        };
+
+    this.update(data, initialState);
+
+}
